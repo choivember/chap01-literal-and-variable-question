c5270990e456b87ffed47c1e0790e66aaa593caf
choivember <pocketmongyo@gmail.com>
2023-05-02 14:00:24 +0900

    사칙 연산 풀이

--- src/main/java/com/ohgiraffers/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application2.java
@@ -8,6 +8,7 @@ public class Application2 {
 		double c = 70.8;
 		System.out.println((int)(a + b + c));
 		System.out.println((int)((a+b+c)/3));
+
 		/* 국어점수 80.5점, 수학점수 50.6점, 영어점수 70.8점을 실수 형태로 저장한 뒤
 		 * 총점과 평균을 정수 형태로 출력하세요
 		 * 
